b42fa6674ef9f53e0a3ca852ee0f2336da384f53
hlassiege <hlassiege@gmail.com>
2024-06-13 17:22:38 +0200

    feat: change frequency for this cron job

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     },
     nitro: {
         scheduledTasks: {
-            '0 * * * *': ['schema'],
+            '* * * * *': ['schema'],
             '0 0 1 * *': ['purge'],
         },
         experimental: {
